7a78da80c74e28c6bf582e4011a88a8ed26f902d
Sep <i@sepblog.me>
2013-03-28 19:12:43 +0800

    Update Text.java Tab

--- Framework.Text/src/main/java/sep/framework/text/Text.java
+++ Framework.Text/src/main/java/sep/framework/text/Text.java
@@ -5,64 +5,67 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public final class Text {
-	public static String format(final CharSequence pattern, final Object... arguments) {
-    	return MessageFormat.format(pattern.toString(), arguments);
-    }
-	
+	public static String format(final String pattern, final Object... arguments) {
+		return MessageFormat.format(pattern, arguments);
+	}
+
 	public static String leftPad(final long value, final short length) {
 		return String.format("%0" + length + "d", value);
-    }
+	}
 
-    public static String leftPad(final String value, final int length) {
-    	return leftPad(value, length, " ");
-    }
+	public static String leftPad(final String value, final int length) {
+		return leftPad(value, length, " ");
+	}
 
-    public static String leftPad(final String value, final int length, final String padContent) {
+	public static String leftPad(final String value, final int length,
+			final String padContent) {
 		if (value != null && value.isEmpty() && value.length() <= length) {
-    		return value;
-    	} else {
-    		final String padChars = (padContent == null || padContent.isEmpty()) ? " " : padContent;
-    		final StringBuffer buffer = new StringBuffer(value);
-    		buffer.append(value);
-    		for (int i = 0; i < length; i++) {
-    			buffer.append(padChars);
+			return value;
+		} else {
+			final String padChars = (padContent == null || padContent.isEmpty()) ? " "
+					: padContent;
+			final StringBuffer buffer = new StringBuffer(value);
+			buffer.append(value);
+			for (int i = 0; i < length; i++) {
+				buffer.append(padChars);
 			}
-    		return buffer.toString();
-    	}
-    }
-    
-    /**
-     * 将字符串的第一个字母改为大写
-     * @param content 要修改的字符串
-     * @return 修改后的字符串
-     */
-    public static String lowerFirst(final String content) {
-    	if (content.length() == 1) {
-    		return content.toLowerCase();
-    	}
-    	
-    	final char[] chars = content.toCharArray();
-    	chars[0] = Character.toLowerCase(chars[0]);
-        return String.valueOf(chars);
-    }
+			return buffer.toString();
+		}
+	}
+
+	/**
+	 * 将字符串的第一个字母改为大写
+	 * 
+	 * @param content 要修改的字符串
+	 * @return 修改后的字符串
+	 */
+	public static String lowerFirst(final String content) {
+		if (content.length() == 1) {
+			return content.toLowerCase();
+		}
+
+		final char[] chars = content.toCharArray();
+		chars[0] = Character.toLowerCase(chars[0]);
+		return String.valueOf(chars);
+	}
 
 	public static String rightPad(final String value, final int length) {
 		return rightPad(value, length, " ");
 	}
 
-    public static String rightPad(final String value, final int length, final String padContent) {
-    	if (value != null && value.isEmpty() && value.length() <= length) {
-    		return value;
-    	} else {
-    		final String padChars = (padContent == null || padContent.isEmpty()) ? " " : padContent;
-    		final StringBuffer buffer = new StringBuffer(value);
-    		for (int i = 0; i < length; i++) {
-    			buffer.append(padChars);
+	public static String rightPad(final String value, final int length, final String padContent) {
+		if (value != null && value.isEmpty() && value.length() <= length) {
+			return value;
+		} else {
+			final String padChars = (padContent == null || padContent.isEmpty()) ? " " : padContent;
+			final StringBuffer buffer = new StringBuffer(value);
+			for (int i = 0; i < length; i++) {
+				buffer.append(padChars);
 			}
-    		buffer.append(value);
-    		return buffer.toString();
-    	}
-    }
+			buffer.append(value);
+			return buffer.toString();
+		}
+	}
 
 	public static boolean isPalindrome(final CharSequence value) {
 		if (value.length() <= 1) {
@@ -80,7 +83,7 @@ public final class Text {
 		}
 		return true;
 	}
-	
+
 	public static boolean isPalindrome(final long number) {
 		long index = number, result = 0;
 		while (index != 0) {
@@ -90,7 +93,7 @@ public final class Text {
 		}
 		return result == number;
 	}
-    
+
 	private Text() {
 	}
 }
