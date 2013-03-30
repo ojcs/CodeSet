211be88f43b877556f21a433f91c22987a5bf818
Sep <i@sepblog.me>
2013-03-30 20:50:12 +0800

    Update Maths

--- Kit/src/main/java/sep/util/Maths.java
+++ Kit/src/main/java/sep/util/Maths.java
@@ -11,79 +11,88 @@ import static java.lang.Math.sin;
 import java.util.Random;
 
 public final class Maths {
+	/** 反三角函数[反余弦] */
 	public static double arccos(final double value) {
 		return acos(value);
 	}
 
+	/** 反三角函数[反余切] */
 	public static double arccot(final double value) {
 		return arctan(1 / value);
 	}
-
+	
+	/** 反三角函数[反余割] */
 	public static double arccsc(final double value) {
 		return arcsin(1 / value);
 	}
-
+	
+	/** 反三角函数[反正割] */
 	public static double arcsec(final double value) {
 		return arccos(1 / value);
 	}
-
+	
+	/** 反三角函数[反正弦] */
 	public static double arcsin(final double value) {
 		return asin(value);
 	}
 
+	/** 反三角函数[反正切] */
 	public static double arctan(final double value) {
 		return atan(value);
 	}
 
+	/** 三角函数[余切] */
 	public static double cot(final double value) {
 		return cos(value) / sin(value);
 	}
 
-	/** 余矢函数 */
+	/** 余矢[余弦] */
 	public static double covercosin(final double value) {
 		return 1 + sin(value);
 	}
 
-	/** 余矢函数 */
+	/** 余矢[正弦] */
 	public static double coversin(final double value) {
 		return 1 - sin(value);
 	}
 
+	/** 三角函数[余割] */
 	public static double csc(final double value) {
 		return 1 / sin(value);
 	}
 
-	/** 外余割函数 */
-	static double excsc(final double value) {
+	/** 外[余割] */
+	public static double excsc(final double value) {
 		return csc(value) - 1;
 	}
 
-	/** 外正割函数 */
-	static double exsec(final double value) {
+	/** 外[正割] */
+	public static double exsec(final double value) {
 		return sec(value) - 1;
 	}
 
-	/** 半余矢函数 */
-	static double hacovercosin(final double value) {
+	/** 半余矢[余弦] */
+	public static double hacovercosin(final double value) {
 		return (1 + sin(value)) / 2;
 	}
 
-	/** 半余矢函数 */
-	static double hacoversin(final double value) {
+	/** 半余矢[正弦] */
+	public static double hacoversin(final double value) {
 		return (1 - sin(value)) / 2;
 	}
 
-	/** 半正矢函数 */
-	static double havercosin(final double value) {
+	/** 半正矢[余弦] */
+	public static double havercosin(final double value) {
 		return (1 + cos(value)) / 2;
 	}
 
-	/** 半正矢函数 */
-	static double haversin(final double value) {
+	/** 半正矢[正弦] */
+	public static double haversin(final double value) {
 		return (1 - cos(value)) / 2;
 	}
 
-	static double log2(final double value) {
+	/** 2的自然对数 */
+	public static double log2(final double value) {
 		return logN(2, value);
 	}
 
@@ -93,7 +102,7 @@ public final class Maths {
 	 * @param real 真数。
 	 * @return 对数值。
 	 */
-	static double logN(final double last, final double real) {
+	public static double logN(final double last, final double real) {
 		return log(real) / log(last);
 	}
 
@@ -131,7 +140,7 @@ public final class Maths {
 	 * @param bit 要保留小数的精确位数。
 	 * @return 舍入后的结果。
 	 */
-	static double round(final double number, final int bit) {
+	public static double round(final double number, final int bit) {
 		double temp = pow(10, bit);
 		return java.lang.Math.round(number * temp) / temp;
 	}
@@ -140,12 +149,12 @@ public final class Maths {
 		return 1 / cos(value);
 	}
 
-	/** 正矢函数 */
+	/** 正矢[余弦] */
 	public static double vercosin(final double value) {
 		return 1 + cos(value);
 	}
 	
-	/** 正矢函数 */
+	/** 正矢[正弦] */
 	public static double versin(final double value) {
 		return 1 - cos(value);
 	}
