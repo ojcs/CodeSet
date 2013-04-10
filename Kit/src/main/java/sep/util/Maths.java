a128dced33f841ddbbb71f73e1284145765a1545
Sep <Sep@Sep-PC>
2013-04-11 05:20:13 +0800

    Clean-up Maths repeat in collection/ArrayUtil

--- Kit/src/main/java/sep/util/Maths.java
+++ Kit/src/main/java/sep/util/Maths.java
@@ -2,6 +2,8 @@ package sep.util;
 
 import static java.lang.Math.pow;
 
+import java.math.BigInteger;
+
 public final class Maths {
 	public static long fibonacci(final long size) {
 		long count = 0, first = 1, second = 1;
@@ -24,106 +26,6 @@ public final class Maths {
 		return true;
 	}
 
-	public static double max(final double... values) {
-		double result = values[0];
-		for (double value : values) {
-			if (result > value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-	
-	public static float max(final float... values) {
-		float result = values[0];
-		for (float value : values) {
-			if (result > value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-	
-	public static int max(final int... values) {
-		int result = values[0];
-		for (int value : values) {
-			if (result > value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-
-	public static long max(final long... values) {
-		long result = values[0];
-		for (long value : values) {
-			if (result > value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-	
-	public static short max(final short... values) {
-		short result = values[0];
-		for (short value : values) {
-			if (result > value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-
-	public static double min(final double... values) {
-		double result = values[0];
-		for (double value : values) {
-			if (result < value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-	
-	public static float min(final float... values) {
-		float result = values[0];
-		for (float value : values) {
-			if (result < value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-	
-	public static int min(final int... values) {
-		int result = values[0];
-		for (int value : values) {
-			if (result < value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-
-	public static long min(final long... values) {
-		long result = values[0];
-		for (long value : values) {
-			if (result < value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-
-	public static short min(final short... values) {
-		short result = values[0];
-		for (short value : values) {
-			if (result < value) {
-				result = value;
-			}
-		}
-		return result;
-	}
-
 	/**
 	 * 四舍五入
 	 * @param number 要进行舍入操作的数。
@@ -133,6 +35,11 @@ public final class Maths {
 	public static double round(final double number, final int bit) {
 		double temp = pow(10, bit);
 		return java.lang.Math.round(number * temp) / temp;
+	}
+	
+	/** 任意进制转换 */
+	public static String convertRadix(long value, final int radix) {
+		return BigInteger.valueOf(value).toString(radix);
 	}
 
 	private Maths() {
