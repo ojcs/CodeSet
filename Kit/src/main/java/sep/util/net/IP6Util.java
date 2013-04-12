7af4c24ff577c903c89f4ef14d9fb79b3209c782
Sep <Sep@Sep-PC>
2013-04-12 18:35:57 +0800

    Update IP6Util

--- Kit/src/main/java/sep/util/net/IP6Util.java
+++ Kit/src/main/java/sep/util/net/IP6Util.java
@@ -1,5 +1,8 @@
 package sep.util.net;
 
+import static java.util.regex.Pattern.compile;
+import static java.util.regex.Pattern.matches;
+
 import java.io.IOException;
 import java.net.Inet6Address;
 import java.net.InetAddress;
@@ -7,9 +10,8 @@ import java.net.NetworkInterface;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Pattern;
 
-import sep.util.collection.Fetch;
+import sep.util.collection.Cursor;
 
 public final class IP6Util {
 	public static boolean checkIPv6Format(final String ip) {
@@ -20,28 +22,28 @@ public final class IP6Util {
 		}
 		return
 		// a valid IPv6 address should contains no less than 1,
-		   (1 < Pattern.compile(":").split(address).length)
+		   (1 < compile(":").split(address).length)
 		// and no more than 7 “:” as separators
-		&& (Pattern.compile(":").split(address).length <= 8)
+		&& (compile(":").split(address).length <= 8)
 		// the address can be compressed, but “::” can appear only once
-		&& (Pattern.compile("::").split(address).length <= 2)
+		&& (compile("::").split(address).length <= 2)
 		// if a compressed address
-		&& (Pattern.compile("::").split(address).length == 2)
+		&& (compile("::").split(address).length == 2)
 		// if starts with “::” – leading zeros are compressed
 		? (((address.substring(0, 2).equals("::"))
-			? Pattern.matches("^::([\\da-f]{1,4}(:)){0,4}(([\\da-f]{1,4}(:)[\\da-f]{1,4})|([\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))",address)
-				: Pattern.matches("^([\\da-f]{1,4}(:|::)){1,5}(([\\da-f]{1,4}(:|::)[\\da-f]{1,4})|([\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))", address)))
+			? matches("^::([\\da-f]{1,4}(:)){0,4}(([\\da-f]{1,4}(:)[\\da-f]{1,4})|([\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))",address)
+				: matches("^([\\da-f]{1,4}(:|::)){1,5}(([\\da-f]{1,4}(:|::)[\\da-f]{1,4})|([\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))", address)))
 				// if ends with "::" - ending zeros are compressed
 				: ((address.substring(address.length() - 2).equals("::"))
-						? Pattern.matches("^([\\da-f]{1,4}(:|::)){1,7}", address) 
-								: Pattern.matches("^([\\da-f]{1,4}:){6}(([\\da-f]{1,4}:[\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))", address));
+						? matches("^([\\da-f]{1,4}(:|::)){1,7}", address) 
+								: matches("^([\\da-f]{1,4}:){6}(([\\da-f]{1,4}:[\\da-f]{1,4})|((\\d{1,3}.){3}\\d{1,3}))", address));
 	}
 
 	public static Set<Inet6Address> getLocalIPv6Addresses() throws IOException {
 		Set<Inet6Address> set = new HashSet<Inet6Address>();
 		final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 		outer: while (interfaces.hasMoreElements()) {
-			for (InetAddress address : new Fetch<InetAddress>(interfaces.nextElement().getInetAddresses())) {
+			for (InetAddress address : new Cursor<InetAddress>(interfaces.nextElement().getInetAddresses())) {
 				if (address instanceof Inet6Address && !IPUtil.isReservedAddr(address)) {
 					set.add((Inet6Address) address);
 					break outer;
