123205a44b69930c9f5b43e9617cbb5e9e97357d
Sep <Sep@Sep-PC>
2013-04-23 00:14:06 +0800

    add UserScrips

---
+++ UserScripts/csdn.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        CSDN免积分下载
+// @description CSDN免积分下载
+// @version     0.0.2
+//
+// @grant       none
+// @author      @Sep
+// @license		MIT License
+// 
+// @include     http://download.csdn.net/detail/*
+// @include     http://download.csdn.net/download/*
+// ==/UserScript==
+(function($){
+	var id = location.pathname.match(/\d+$/)[0];
+	
+	$('.res_info .info').after('<a href="javascript:void(0)"><h1>免积分下载</h1></a>').next().click(function(){
+		if (this.href == 'javascript:void(0)') {
+			if (!localStorage[id]) {
+				$.ajax({
+					type : 'get', async : false, dataType : 'JSON',
+					url  : 'http://download.csdn.net/index.php/rest/source/getsourceinfo/' + id,
+					
+					success : function(data)  { localStorage[id] = JSON.parse(data).url; },
+					error   : function(error) { alert('免积分下载,下载地址获取失败.'); }
+				});
+			}
+			$(this).attr('href', localStorage[id]);
+		}
+	});
+})(jQuery);
