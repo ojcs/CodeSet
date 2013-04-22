4d9a8523ce7c63498b432a7925c98198a84928d7
Sep <Sep@Sep-PC>
2013-04-23 01:31:12 +0800

    update

--- UserScripts/CSDNFreeDownload.user.js
+++ UserScripts/CSDNFreeDownload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        CSDN免积分下载
 // @description CSDN免积分下载
-// @version     0.0.6
+// @version     0.0.7
 //
 // @grant       none
 // @author      @Sep
@@ -13,8 +13,9 @@
 (function($){
 	$('.res_info .info').after('<a href="javascript:void(0)"><h1>免积分下载</h1></a>').next().click(function(){
 		if (this.href == 'javascript:void(0)') {
-			var $this = $(this), id = location.pathname.match(/\d+$/)[0];;
+			var $this = $(this), id = location.pathname.match(/\d+$/)[0];
 			$.getJSON('http://download.csdn.net/index.php/rest/source/getsourceinfo/' + id, function(data){
+				alert(data);
 				$this.attr('href', JSON.parse(data).url);
 			}).fail(function(){
 				alert('免积分下载地址获取失败。');
