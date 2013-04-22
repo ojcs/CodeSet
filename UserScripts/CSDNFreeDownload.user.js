d1bd5f6aeb8e1170102621301bb33fc942465810
Sep <Sep@Sep-PC>
2013-04-23 01:25:04 +0800

    update

--- UserScripts/CSDNFreeDownload.user.js
+++ UserScripts/CSDNFreeDownload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        CSDN免积分下载
 // @description CSDN免积分下载
-// @version     0.0.4
+// @version     0.0.5
 //
 // @grant       none
 // @author      @Sep
@@ -10,24 +10,16 @@
 // @include     http://download.csdn.net/detail/*
 // @include     http://download.csdn.net/download/*
 // ==/UserScript==
-(function($, id){
+(function($){
 	$('.res_info .info').after('<a href="javascript:void(0)"><h1>免积分下载</h1></a>').next().click(function(){
 		if (this.href === 'javascript:void(0)') {
-			var $this = $(this);
-			$.ajax({
-				url  : 'http://download.csdn.net/index.php/rest/source/getsourceinfo/' + id,
-				type : 'get',
-				async : false,
-				dataType : 'JSON',
-
-				success : function(json) {
-					$this.attr('href', JSON.parse(json).url);
-				},
-				error : function() {
-					alert('免积分下载地址获取失败。');
-					$this.remove();
-				}
+			var $this = $(this), id = location.pathname.match(/\d+$/)[0];;
+			$.getJSON('http://download.csdn.net/index.php/rest/source/getsourceinfo/' + id, function(data){
+				$this.attr('href', JSON.parse(data).url);
+			}).fail(function(){
+				alert('免积分下载地址获取失败。');
+				$this.remove();
 			});
 		}
 	});
-})(jQuery, location.pathname.match(/\d+$/)[0]);
+})(jQuery);
