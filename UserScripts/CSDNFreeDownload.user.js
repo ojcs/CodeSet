db375ea889e5a79f661d0bfab90047988c1d15e4
Sep <Sep@Sep-PC>
2013-04-24 21:49:18 +0800

    update to 0.1.5

--- UserScripts/CSDNFreeDownload.user.js
+++ UserScripts/CSDNFreeDownload.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        CSDN免积分下载
 // @description 免积分下载
-// @version     0.1.4
+// @version     0.1.5
 //
 // @grant       none
 //
-// @downloadURL    https://github.com/Septembers/CodeSet/raw/master/UserScripts/CSDNFreeDownload.user.js
+// @downloadURL	https://github.com/Septembers/CodeSet/raw/master/UserScripts/CSDNFreeDownload.user.js
 //
 // @include     http://download.csdn.net/detail/*
 // @include     http://download.csdn.net/download/*
@@ -14,13 +14,17 @@
 	$('.res_info .info').after('<a href="javascript:void(0);"><h1>获取免积分下载地址</h1></a>').next().click(function() {
 		if (this.href.length === 19) {
 			var $this = $(this);
-			var url = '//download.csdn.net/index.php/rest/source/getsourceinfo/' + location.pathname.match(/\d+$/)[0];
-			$.getJSON(url, function(data) {
-				$this.attr('href', data.url).html('<h1>免积分下载</h1>');
+			$.ajax({
+				url : '//download.csdn.net/index.php/rest/source/getsourceinfo/' + location.pathname.match(/\d+$/)[0],
+				
+				dataType : 'json',
+				success : function(data) {
+					$this.attr('href', data.url).html('<h1>免积分下载</h1>');
+				},
+				error : function() {
+					$this.removeAttr('href').html('<h1>免积分下载地址获取失败。</h1>');
+				}
 			});
-			if (this.href.length === 19) {
-				$this.removeAttr('href').html('<h1>免积分下载地址获取失败。</h1>');
-			}
 		}
 	});
 })(jQuery);
